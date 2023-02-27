063bb30441005216fe13103fd15fb462d62ca0e1
shajjad Shagor <shajjadshagor@gmail.com>
2023-02-27 12:25:44 +0600

    design for desktop menubar and design it and set hemburger menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
+import { FaShoppingCart } from "react-icons/fa";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+
+const logo = (
+  <div className={styles.logo}>
+    <Link to="/">
+      <h2>
+        e<span>Shop</span>.
+      </h2>
+    </Link>
+  </div>
+);
+
+const cart = (
+  <span className={styles.cart}>
+    <Link to="/cart">
+      Cart <FaShoppingCart size={20} />
+      <p className="">0</p>
+    </Link>
+  </span>
+);
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const hideMenu = () => {
+    setShowMenu(false);
+  };
+
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <div className={styles.header}>
+        {logo}
+        <nav
+          className={
+            showMenu ? `${styles["show-nav"]}` : `${styles["hide-nav"]}`
+          }
+        >
+          <div
+            className={
+              showMenu
+                ? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}`
+                : `${styles["nav-wrapper"]}`
+            }
+            onClick={hideMenu}
+          >
+            {" "}
+          </div>
+          <ul onClick={hideMenu}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+          <div className={styles["header-right"]} onClick={hideMenu}>
+            <span className={styles.links}>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+              <Link to="/order-history">My Orders</Link>
+            </span>
+            {cart}
+          </div>
+        </nav>
+
+        <div className={styles["menu-icon"]}>
+          {cart}
+          <HiOutlineMenuAlt3 size={28} onClick={toggleMenu} />
+        </div>
+      </div>
+    </header>
   );
 };
 
